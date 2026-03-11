5830389f8ddb236ee1165a842e5e3fe17d12584c
florenz <florenz@tju.edu.cn>
2026-03-11 23:22:53 +0800

    [Fix] Leave proxy unhandled. If proxy is needed, flaresolverr is available only and only if system-proxy is on.

--- django_backend/flaresolverr.sh
+++ django_backend/flaresolverr.sh
@@ -3,6 +3,5 @@ docker run -d \
     --name=flaresolverr \
     -p 8191:8191 \
     -e LOG_LEVEL=info \
-    -e BROWSER_PROXY_SERVER=http://host.docker.internal:7077 \
     --restart unless-stopped \
     ghcr.io/flaresolverr/flaresolverr:latest
