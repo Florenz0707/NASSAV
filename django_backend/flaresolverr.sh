5f8488d6d40bf57a952af0820a022e1f726129d4
florenz <florenz@tju.edu.cn>
2026-03-11 22:45:49 +0800

    [Fix] Fix bug of not accessing flaresolverr.

--- django_backend/flaresolverr.sh
+++ django_backend/flaresolverr.sh
@@ -1,6 +1,8 @@
+docker rm -f flaresolverr 2>/dev/null || true
 docker run -d \
-      --name=flaresolverr \
-        -p 8191:8191 \
-          -e LOG_LEVEL=info \
-            --restart unless-stopped \
-              ghcr.io/flaresolverr/flaresolverr:latest
+    --name=flaresolverr \
+    -p 8191:8191 \
+    -e LOG_LEVEL=info \
+    -e BROWSER_PROXY_SERVER=http://host.docker.internal:7077 \
+    --restart unless-stopped \
+    ghcr.io/flaresolverr/flaresolverr:latest
